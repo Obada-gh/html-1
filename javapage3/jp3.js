e8f9abb6adaf56287eb60d5d3c4be8c5f16719c2
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-02-24 23:16:33 +0200

    java3

---
+++ javapage3/jp3.js
@@ -0,0 +1,48 @@
+
+    var name="ronaldo";
+    var age=27;
+    var team="juventus";
+    var esport=prompt("can you press ok ?");
+    var Cups=4
+    var uCups=prompt("how many cups ronaldo have per the year? the answer 4");
+    if (Cups !=uCups) {
+while (Cups !=uCups) { 
+    alert("nope !");
+    uCups=prompt("how many cups ronaldo have per the year? the answer 4");
+}
+    }
+    else {
+        alert("good job !");
+      }
+
+
+var age=27;
+var team="juventus";
+function myFunction(p1) {
+    return p1 * 10;
+
+
+}
+var Cups = myFunction(4)
+
+ {
+    document.getElementById("demo").innerHTML = " best player :   " + name+ " / age:  " + age + "  / Cups for the last 10 years: " + Cups;
+  }
+
+  function add_img() { 
+	var img = document.createElement('img'); 
+    img.src = 'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png'; 
+	document.getElementById("imgg").appendChild(img);
+}
+
+{
+    document.getElementById("imgg").innerHTML = add_img()
+  }
+  
+
+
+
+
+
+   
+      
