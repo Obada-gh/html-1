2db1fa95b64435dd4c9dbe42c231854df8cb151a
Obada-gh <obada7jaber7@gmail.com>
2021-02-28 22:47:06 +0200

    add the gists folder

---
+++ gists/app.js
@@ -0,0 +1,14 @@
+
+
+let a = prompt("do you have a gaming pc ?");
+let b = prompt("do you have console ?");
+let c = prompt("do you want to make a setup");
+let d = prompt("do you want to bulid your own pc ?");
+
+alert(" 1- " + a + " 2- " +  b + " 3- " + c + " 4- " + d +  " (your 4 ans)");
+
+console.log(a +" "+ "we want to know if he a gamer");
+console.log(a +" "+ "we want to know if he a console gamer");
+console.log(a +" "+ "we want to know if he want to make a new setup");
+console.log(a +" "+ "we want to know if he want to buy a new parts");
+
